9d4de26348fb0013ed94c698bc403c4b985f0f97
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-06-28 20:03:20 +0530

    chore: initialize frontend with create-react-app and implement basic pipeline UI with react flow

---
+++ frontend/src/submit.js
@@ -0,0 +1,8 @@
+export const SubmitButton = () => {
+
+    return (
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <button type="submit">Submit</button>
+        </div>
+    );
+}
